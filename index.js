856de4e425d5387881baedd3d20d012e719211d5
github username <juliannne@gmail.com>
2018-01-16 18:15:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '40d0bd03f2ec0e4dcd017c1b6949b804bcd5a793'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
